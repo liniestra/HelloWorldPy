d43bf7515574738ffdf15710bde7caad363e31f8
Luis Iniestra <liniestra@gmail.com>
2026-05-19 16:36:56 -0600

    Se agregó el archivo p3input.py que permite hacer uso de los input para interactuar con el usuario

---
+++ p3input.py
@@ -0,0 +1,6 @@
+nombre = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+edad = input("¿Cuál es tu edad? ")
+ciudad = input("¿Cuál es tu ciudad? ")
+
+print("Hola, tu nombre es", nombre, apellido, "tienes", edad, "años. Vives en", ciudad)
