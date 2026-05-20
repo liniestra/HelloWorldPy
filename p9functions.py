5e9e3f5df63246559b1bf95507e703da3422c400
Luis Iniestra <liniestra@gmail.com>
2026-05-19 18:10:23 -0600

    Se agregó el archivo p9functions.py para practicar el uso de las funciones en python, se usan tres funciones, una para decir hola, para preguntar cómo está y para decir adiós

---
+++ p9functions.py
@@ -0,0 +1,13 @@
+def saludar(nombre):
+    print("Hola " + nombre + ", ¡bienvenido!")
+
+def como(nombre):
+    print("¿Cómo estás " + nombre + "?")
+
+def despedir(nombre):
+    print("Adiós " + nombre + ", ¡nos vemos pronto!")
+
+tu_nombre = input("¿Cómo te llamas?")
+saludar(tu_nombre)
+como(tu_nombre)
+despedir(tu_nombre)
