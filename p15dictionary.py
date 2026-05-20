55d366d040bb6f5d6d6361b5a258d4a613ad99d5
Luis Iniestra <liniestra@gmail.com>
2026-05-19 19:56:28 -0600

    En esta práctica p15dictionary.py se revisa el concepto de diccionario y se muestra cómo acceder a cada atributo de ese diccionario.

---
+++ p15dictionary.py
@@ -0,0 +1,11 @@
+alumno = {
+    "nombre": "Luis",
+    "apellido": "García",
+    "edad": 20,
+    "curso": "Programación"
+}
+
+print(alumno["nombre"])
+print(alumno["apellido"])
+print(alumno["edad"])
+print(alumno["curso"])
