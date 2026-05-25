a92d7a8d539dffe825baa6ef7c53c416f0b279e4
Luis Iniestra <liniestra@gmail.com>
2026-05-25 16:31:35 -0600

    Se agrega el archivo p29wordcounter.py que al introducir un texto, cuenta el número de palabras y también el número de caracteres de dicho texto.

---
+++ p29wordcounter.py
@@ -0,0 +1,4 @@
+text = input("Introduce un texto y te diré cuántas palabras tiene, también te diré cuántos caracteres tiene.")
+palabras = text.split()
+print("El texto tiene", len(palabras), "palabras.")
+print("El texto tiene", len(text), "caracteres.")
