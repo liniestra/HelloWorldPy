c7fd11bc591b307dd4518ff0bf109d280cc92a02
Luis Iniestra <liniestra@gmail.com>
2026-05-25 18:15:20 -0600

    Se agrega el archivo p35search.py, el cual recibe dos entradas, la primera corresponde al texto completo; mientras que la segunda corresponde a una palabra que se desea buscar en ese texto. El programa es muy sencillo pues simplemente en un if se pregunta si el texto completo contiene la palabra buscada, de ser así imprime que el texto fue encontrado.

---
+++ p35search.py
@@ -0,0 +1,7 @@
+texto = input("Introduce el texto completo:\n")
+buscar = input("Introduce el texto a buscar:\n")
+
+if buscar in texto:
+    print("Texto encontrado.")
+else:
+    print("Texto no encontrado.")
