a3ab6a54ffd5e1b282015694059870250e88ae06
Luis Iniestra <liniestra@gmail.com>
2026-05-25 18:10:18 -0600

    Se agrega el archivo p34mainmenu.py, el cual es un simple menú de sistema sin funcionalidad.

---
+++ p34mainmenu.py
@@ -0,0 +1,20 @@
+while True:
+
+    print("\n=== SISTEMA ===")
+    print("1. Usuarios")
+    print("2. Configuración")
+    print("3. Salir")
+
+    opcion = input("Opción: ")
+
+    if opcion == "1":
+        print("Entraste a usuarios")
+
+    elif opcion == "2":
+        print("Entraste a configuración")
+
+    elif opcion == "3":
+        break
+
+    else:
+        print("Opción inválida")
